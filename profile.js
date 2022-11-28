844ac17bc6e7a80a4a4735750a9414830000969a
tmouton1 <tamia.mouton1@gmail.com>
2022-11-28 15:10:43 -0500

    Tamia Resubmit1128

---
+++ profile.js
@@ -0,0 +1,25 @@
+
+const button = document.querySelector('#color-button');
+
+color.addEventListener('click',
+() => {
+alert('fluorescent beige');
+});
+
+
+const button2 = document.querySelector('#place-button');
+
+place.addEventListener('click',
+() => {
+alert('Negril!');
+});
+
+
+const button3 = document.querySelector('#ritual-button');
+
+ritual.addEventListener('click',
+() => {
+alert('seance!');
+});
+
+
